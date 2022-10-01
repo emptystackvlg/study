000af91228305209014cd1a850f2220dd323908c
Krykov Alexandr <57997351+emptystackvlg@users.noreply.github.com>
2022-10-01 18:21:02 +0300

    Upload

---
+++ C#/Laba_1/Laba_1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Laba_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxh = new System.Windows.Forms.TextBox();
+            this.listBoxTab = new System.Windows.Forms.ListBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(601, 128);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(569, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите начальное значение a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(569, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите начальное значение b";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(601, 250);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxB.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(605, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Введите шаг h";
+            // 
+            // textBoxh
+            // 
+            this.textBoxh.Location = new System.Drawing.Point(601, 371);
+            this.textBoxh.Name = "textBoxh";
+            this.textBoxh.Size = new System.Drawing.Size(100, 20);
+            this.textBoxh.TabIndex = 4;
+            // 
+            // listBoxTab
+            // 
+            this.listBoxTab.FormattingEnabled = true;
+            this.listBoxTab.Location = new System.Drawing.Point(156, 128);
+            this.listBoxTab.Name = "listBoxTab";
+            this.listBoxTab.Size = new System.Drawing.Size(297, 264);
+            this.listBoxTab.TabIndex = 6;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(610, 422);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(91, 28);
+            this.buttonStart.TabIndex = 7;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 485);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.listBoxTab);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxh;
+        private System.Windows.Forms.ListBox listBoxTab;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ C#/Laba_1/Laba_1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            var a = Convert.ToDouble(textBoxA.Text);
+            var b = Convert.ToDouble(textBoxB.Text);
+            var h = Convert.ToDouble(textBoxh.Text);
+            double y;
+            listBoxTab.Items.Add("x\t\t\ty");
+            for (double x = a; x < b; x += h) 
+            {
+                if (x >= 0.9)
+                {
+                    //y = 1/ Math.Pow((0.1+x),2);
+                    y = 3;
+                }
+                else  
+                { 
+                }
+                if (x >= 0  && x < 0.9)
+                {
+                    y = 0.2 * x  + 0.1;
+                }
+                else 
+                {
+                    y = Math.Pow(x,2) + 0.2;
+                }
+
+                listBoxTab.Items.Add(Math.Round(x, 2).ToString() + "\t\t\t" +Math.Round(y, 2).ToString()) ;
+            }
+        }
+    }
+}
